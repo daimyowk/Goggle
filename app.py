b77953a44ffbb398991df8c52dba76d3203fa9ac
kahsoonyap <kahsoonyap1998@hotmail.com>
2015-11-29 20:50:31 -0500

    Some flask stuff

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import utils
 
 app=Flask(__name__)
 
-@app.route('/')
-
+@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        return render_template("result.html", answer=utils.find_answer(request.form["query"]))
+    
 if __name__=="__main__":
     app.debug = True
     app.run('0.0.0.0', port=8000)
